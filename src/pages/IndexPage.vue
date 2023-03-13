54ee140b57f0bb7f644bd0f681e240c13244fd50
Simone Willemse <simywillemse@gmail.com>
2023-03-13 18:29:12 +0100

    added cursor background element

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app id="homePage">
     <q-page class="flex flex-center">
-      <img id="indexLogo" src="/img/estuera.svg?import." />
-
+      <img id="indexLogo" src="/img/estuera.svg" />
       <v-container fluid>
-        <font-awesome-icon icon="fa-solid fa-user-secret" />
+        <!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
         <v-row>
+          <div class="cursor"></div>
+          <div class="blur"></div>
           <div id="about-container">
             <AboutSection />
           </div>
@@ -25,7 +26,8 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import gsap from "gsap";
 import AboutSection from "../components/AboutSection.vue";
 import VideoSection from "../components/VideoSection.vue";
 import DiscoSection from "src/components/DiscoSection.vue";
@@ -39,5 +41,18 @@ export default defineComponent({
     DiscoSection,
     ContactSection,
   },
+});
+gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
+
+let cursor = document.querySelector(".cursor");
+
+let mouseX;
+let mouseY;
+
+window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    gsap.to(cursor, 0.5, { x: mouseX, y: mouseY });
 });
 </script>
